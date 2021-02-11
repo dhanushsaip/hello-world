d62fb878acf8b690a9df584b0d143354d55b02a6
sai dhanush <saidhanushtecno@ip-172-31-9-114.ec2.internal>
2021-02-11 04:36:06 +0000

    First Commit

---
+++ src/test/firstclass.java
@@ -0,0 +1,14 @@
+package test;
+
+public class firstclass {
+
+	public firstclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("hello world");
+	}
+
+}
